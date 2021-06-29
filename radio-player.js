a3917c0f8aefaf69f0c414c2c974cc8c507e7c58
webdcr-radio <86690913+webdcr-radio@users.noreply.github.com>
2021-06-29 18:11:25 -0400

    Create radio-player.js

---
+++ radio-player.js
@@ -0,0 +1,53 @@
+playing = 0;
+// custom audio player settings:
+
+// variables:
+var radioURL = 'http://wdcr.dartmouth.edu:8000/;';
+var playImage = 'https://webdcr.com/wp-content/uploads/icons-09.png';
+var pauseImage = 'https://webdcr.com/wp-content/uploads/icons-16.png';
+var loadingImage = 'https://webdcr.com/wp-content/uploads/icons-11.png';
+
+var playing = 0 // track state of audio player (0=paused,1=loading,2=playing)
+var radioAud; // initializing an audio object (to be filled when audio player starts)
+
+function radioClick() {
+	if (radioAud === undefined) {
+		radioAud = new Audio(radioURL);
+		radioAud.addEventListener("waiting", radioLoadingActions);	
+		radioAud.addEventListener("playing", radioPlayActions);
+		radioAud.addEventListener("pause", radioPauseActions);
+	}
+	if (playing == 2 || playing == 1) {
+		radioAud.pause()
+	} else {
+		radioAud.load(); // update to latest music
+		radioAud.play() // play it!
+	}
+}
+
+function radioLoadingActions() {
+	playing = 1;
+	$( ".radioPlayer" ).attr("src",loadingImage);
+	$( ".radioPlayer" ).css("animation","spin 2s linear infinite");
+}
+
+function radioPlayActions() {
+	playing = 2;
+	$( ".radioPlayer" ).attr("src",pauseImage);
+	$( ".radioPlayer" ).css("animation","")
+}
+
+function radioPauseActions() {
+	playing = 0;
+	$( ".radioPlayer" ).attr("src",playImage);
+	$( ".radioPlayer" ).css("animation","");
+}
+
+function radioCheck() {
+	if (radioChecked == false) {
+		if (playing == 0) {radioPauseActions()}
+		if (playing == 1) {radioLoadingActions()}
+		if (playing == 2) {radioPlayActions()}	
+		radioChecked = true;
+	}
+}
