afb1c6ec5df9ed7427384c43a6faf9bcddc02f2b
Ray Crist <rayhcrist@gmail.com>
2022-01-16 12:11:30 -0500

    Update header.js

--- header.js
+++ header.js
@@ -1,5 +1,7 @@
 // start AJAX functions below: 
 var radioChecked = false;
+SHORT_CODE_LINK = "https://webdcr.com/wp-admin/admin-ajax.php?action=radio_station_current_show&instance=1&timestamp=1642352758&title=&display_hosts=0&show_avatar=1&show_title=1&show_link=1&default_name=WebDCR%20Automation%20Now%20Playing.&time=12&show_sched=1&show_playlist=0&show_all_sched=1&show_desc=0&avatar_width=&avatar_size=thumbnail&title_position=center&link_hosts=0&show_encore=1&countdown=0&ajax=on&dynamic=0&widget=0&id=&for_time=0
+
 
 function modifyJOAN() {
 	joanScript = $( "script[src*='joan']" );
@@ -77,7 +79,8 @@ function actionsOnLoad(linkPath) {
 		history.pushState(null, null, linkPath); // make it look like we changed pages in page history
 		managePostsNavigation(linkPath);
   }
-	setTimeout(checkJoan, 3000);
+	$("#rs-current-show-1-loader").attr('src', SHORT_CODE_LINK);
+	// setTimeout(checkJoan, 3000);
 }
 
 
