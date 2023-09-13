fe6108917c2a67aeaee2a8806a2cc222956e9d69
V Quidore <veronica.quidore@gmail.com>
2023-09-13 12:05:10 -0400

    new link plz plz plz work

--- radio-player.js
+++ radio-player.js
@@ -2,7 +2,7 @@ playing = 0;
 // custom audio player settings:
 
 // variables:
-var radioURL = 'https://playerservices.streamtheworld.com/api/livestream-redirect/DC_RADIO.mp3/;';
+var radioURL = 'https://19293.live.streamtheworld.com/DC_RADIO.mp3';
 var playImage = 'https://webdcr.com/wp-content/uploads/icons-09.png';
 var pauseImage = 'https://webdcr.com/wp-content/uploads/icons-16.png';
 var loadingImage = 'https://webdcr.com/wp-content/uploads/icons-11.png';
